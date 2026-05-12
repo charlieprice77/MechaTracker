32ae730336b67fc25edb969353f86a42ab7e1018
charlie-amua <charlie@amua.co>
2026-05-12 16:48:53 +1200

    feat: add small script for helping to better understand unit drop ids
    Unit drop id prefix is actually just '3' and round number is always 2
    digits with leading 0's for single digit round

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from mechatracker.parser import *
+from glob import glob
+
+import xml.etree.ElementTree as ET
 
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting mechatracker...")
-    load_replay_bytes("ReplayFiles/2013_20260415--201415094_[Charlie]VS[BobVance].grbr")
+    parse_replay("ReplayFiles/2013_20260415--201415094_[Charlie]VS[BobVance].grbr")
 
+    # for path in glob("ReplayFiles/*.grbr"):
+    #     root = ET.fromstring(load_replay_bytes(path))
+    #     # for child in root:
+    #     #     print(child.tag, child.attrib)
+    #     #     for nested_child in child:
+    #     #         print("    ", nested_child.tag, nested_child.attrib)
+    #     snapData = root.findall('matchDatas/MatchSnapshotData')
+    #     for snap in snapData:
+    #         reinforceItems = (snap.find('reinforceItems'))
+    #         if reinforceItems is None:
+    #             continue
+    #         if len(reinforceItems) > 1:
+    #             arrayOfInts = reinforceItems[1]
+    #             for id in arrayOfInts:
+    #                 id = id.text
+    #                 if id[0] == '3':
+    #                     continue
+    #                 else:
+    #                     print(path)
+    #                     print(id)
 
 if __name__ == "__main__":
     main()
